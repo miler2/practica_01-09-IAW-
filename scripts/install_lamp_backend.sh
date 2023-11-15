35b68512f48b450e10326abf9643a5dd615af528
Ubuntu <ubuntu@ip-172-31-91-114.ec2.internal>
2023-11-15 13:16:24 +0000

    He añadido los cambios en los permisos de los dos ejecutables y añadido cosas en install backend

--- scripts/install_lamp_backend.sh
+++ scripts/install_lamp_backend.sh
@@ -9,5 +9,11 @@ sudo apt update
 #Actualizamos los paquetes
 #sudo apt upgrade -y
 
+#Importamos el archivo .env
+source .env
+
 #Instalamos el sistema gestor de bases de datos MySQL
 sudo apt install mysql-server -y
+
+#Configuramos MySQL para que sólo acepte conexiones desde la IP privada
+sed -i "s/127.0.0.1/$MYSQL_PRIVATE_IP/" /etc/mysql/mysql.conf.d/mysqld.cnf
